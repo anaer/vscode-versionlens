ce4b326459884d235730b23acf9392156dcfea26
pflannery <61407-pflannery@users.noreply.gitlab.com>
2023-01-31 23:10:41 +0000

    Moved jspm support in to the npm provider

--- src/infrastructure/providers/jspm/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './src/jspmConfig'
-export * from './src/jspmPackageParser'
-export * from './src/jspmSuggestionProvider'
-export * from './src/jspmContainer'
--- src/presentation.extension/versionLensContainer.ts
+++ src/presentation.extension/versionLensContainer.ts
@@ -113,7 +113,6 @@ export async function configureContainer(
       'composer',
       'dotnet',
       'dub',
-      'jspm',
       'maven',
       'npm',
       'pub',
