7b010d21377cafe3103aab82f66af0589e29fbb7
pflannery <61407-pflannery@users.noreply.gitlab.com>
2023-02-09 14:57:15 +0000

    Unwrapped promises to async await in NuGetPackageClient

--- src/infrastructure/providers/dotnet/src/clients/nugetPackageClient.ts
+++ src/infrastructure/providers/dotnet/src/clients/nugetPackageClient.ts
@@ -15,18 +15,11 @@ import {
   VersionHelpers
 } from 'domain/packages';
 import { createSuggestions, SuggestionFactory } from 'domain/suggestions';
-import { DotNetVersionSpec } from '../definitions/dotnet';
 import { NuGetClientData } from '../definitions/nuget';
 import { DotNetConfig } from '../dotnetConfig';
 import { parseVersionSpec } from '../dotnetUtils';
 
 export class NuGetPackageClient implements IPackageClient<NuGetClientData> {
-
-  config: DotNetConfig;
-
-  jsonClient: IJsonHttpClient;
-
-  logger: ILogger;
 
   constructor(config: DotNetConfig, client: IJsonHttpClient, logger: ILogger) {
     this.config = config;
@@ -34,133 +27,136 @@ export class NuGetPackageClient implements IPackageClient<NuGetClientData> {
     this.logger = logger;
   }
 
-  fetchPackage(request: TPackageClientRequest<NuGetClientData>): Promise<TPackageClientResponse> {
-    const requestedPackage = request.dependency.package;
-    const dotnetSpec = parseVersionSpec(requestedPackage.version);
-    return this.fetchPackageRetry(request, dotnetSpec);
-  }
+  config: DotNetConfig;
 
-  fetchPackageRetry(
-    request: TPackageClientRequest<NuGetClientData>,
-    dotnetSpec: DotNetVersionSpec
+  jsonClient: IJsonHttpClient;
+
+  logger: ILogger;
+
+  async fetchPackage(
+    request: TPackageClientRequest<NuGetClientData>
   ): Promise<TPackageClientResponse> {
     const urls = request.clientData.serviceUrls;
     const autoCompleteUrl = urls[request.attempt];
 
-    return this.createRemotePackageDocument(autoCompleteUrl, request, dotnetSpec)
-      .catch((error: HttpClientResponse) => {
+    try {
+      return await this.createRemotePackageDocument(autoCompleteUrl, request);
+    }
+    catch (error) {
+      const errorResponse = error as HttpClientResponse;
 
-        this.logger.debug(
-          "Caught exception from %s: %O",
+      this.logger.debug(
+        "Caught exception from %s: %O",
+        PackageClientSourceType.Registry,
+        errorResponse
+      );
+
+      // increase the attempt number
+      request.attempt++;
+
+      // only retry if 404 and we have more urls to try
+      if (errorResponse.status === 404 && request.attempt < urls.length) {
+        // retry
+        return this.fetchPackage(request);
+      }
+
+      // attempt to create a suggestion from the http status
+      const suggestion = SuggestionFactory.createFromHttpStatus(errorResponse.status);
+      if (suggestion != null) {
+        return ClientResponseFactory.create(
           PackageClientSourceType.Registry,
-          error
+          errorResponse,
+          [suggestion]
         );
+      }
 
-        // increase the attempt number
-        request.attempt++;
-
-        // only retry if 404 and we have more urls to try
-        if (error.status === 404 && request.attempt < urls.length) {
-          // retry
-          return this.fetchPackageRetry(request, dotnetSpec)
-        }
-
-        const suggestion = SuggestionFactory.createFromHttpStatus(error.status);
-        if (suggestion != null) {
-          return ClientResponseFactory.create(
-            PackageClientSourceType.Registry,
-            error,
-            [suggestion]
-          )
-        }
-
-        // unexpected
-        return Promise.reject(error);
-      });
-
+      // unexpected
+      return Promise.reject(errorResponse);
+    };
   }
 
-  createRemotePackageDocument(
+  async createRemotePackageDocument(
     url: string,
-    request: TPackageClientRequest<NuGetClientData>,
-    dotnetSpec: DotNetVersionSpec
+    request: TPackageClientRequest<NuGetClientData>
   ): Promise<TPackageClientResponse> {
 
     const query = {};
     const headers = {};
     const requestedPackage = request.dependency.package;
-    const packageUrl = UrlHelpers.ensureEndSlash(url) + `${requestedPackage.name.toLowerCase()}/index.json`;
+    const packageUrl = UrlHelpers.ensureEndSlash(url)
+      + `${requestedPackage.name.toLowerCase()}/index.json`;
 
-    return this.jsonClient.request(
+    const httpResponse = await this.jsonClient.request(
       HttpClientRequestMethods.get,
       packageUrl,
       query,
       headers
-    )
-      .then((httpResponse) => {
+    );
 
-        const { data } = httpResponse;
+    const { data } = httpResponse;
 
-        const source = PackageClientSourceType.Registry;
+    const source = PackageClientSourceType.Registry;
 
-        const packageInfo = data;
+    const packageInfo = data;
 
-        const responseStatus = {
-          source: httpResponse.source,
-          status: httpResponse.status,
-        };
+    const responseStatus = {
+      source: httpResponse.source,
+      status: httpResponse.status,
+    };
 
-        // sanitize to semver only versions
-        const rawVersions = VersionHelpers.filterSemverVersions(packageInfo.versions);
+    // parse nuget range expressions
+    const dotnetSpec = parseVersionSpec(requestedPackage.version);
 
-        // seperate versions to releases and prereleases
-        const { releases, prereleases } = VersionHelpers.splitReleasesFromArray(
-          rawVersions,
-          this.config.prereleaseTagFilter
-        );
+    // four segment is not supported
+    if (dotnetSpec.spec && dotnetSpec.spec.hasFourSegments) {
+      return ClientResponseFactory.create(
+        PackageClientSourceType.Registry,
+        httpResponse,
+        [],
+      );
+    }
 
-        // four segment is not supported
-        if (dotnetSpec.spec && dotnetSpec.spec.hasFourSegments) {
-          return ClientResponseFactory.create(
-            PackageClientSourceType.Registry,
-            httpResponse,
-            [],
-          )
-        }
+    // sanitize to semver only versions
+    const rawVersions = VersionHelpers.filterSemverVersions(packageInfo.versions);
 
-        // no match if null type
-        if (dotnetSpec.type === null) {
-          return ClientResponseFactory.createNoMatch(
-            source,
-            PackageVersionType.Version,
-            ClientResponseFactory.createResponseStatus(httpResponse.source, 404),
-            // suggest the latest release if available
-            releases.length > 0 ? releases[releases.length - 1] : null,
-          )
-        }
+    // seperate versions to releases and prereleases
+    const { releases, prereleases } = VersionHelpers.splitReleasesFromArray(
+      rawVersions,
+      this.config.prereleaseTagFilter
+    );
 
-        const versionRange = dotnetSpec.resolvedVersion;
+    // return no match if null type
+    if (dotnetSpec.type === null) {
+      return ClientResponseFactory.createNoMatch(
+        source,
+        PackageVersionType.Version,
+        ClientResponseFactory.createResponseStatus(httpResponse.source, 404),
+        // suggest the latest release if available
+        releases.length > 0 ? releases[releases.length - 1] : null,
+      );
+    }
 
-        const resolved = {
-          name: requestedPackage.name,
-          version: versionRange,
-        };
+    const versionRange = dotnetSpec.resolvedVersion;
 
-        // analyse suggestions
-        const suggestions = createSuggestions(
-          versionRange,
-          releases,
-          prereleases
-        );
+    const resolved = {
+      name: requestedPackage.name,
+      version: versionRange,
+    };
 
-        return {
-          source,
-          responseStatus,
-          type: dotnetSpec.type,
-          resolved,
-          suggestions,
-        };
-      });
+    // analyse suggestions
+    const suggestions = createSuggestions(
+      versionRange,
+      releases,
+      prereleases
+    );
+
+    return {
+      source,
+      responseStatus,
+      type: dotnetSpec.type,
+      resolved,
+      suggestions,
+    };
   }
 
 }
