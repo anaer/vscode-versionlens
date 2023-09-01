9d207c1e9dd2f81b560dde6665e10285c144f9ce
pflannery <61407-pflannery@users.noreply.gitlab.com>
2023-09-01 23:06:22 +0100

    Moved defaultReplaceFn to domain\suggestions\utils folder

--- src/domain/suggestions/index.ts
+++ src/domain/suggestions/index.ts
@@ -5,5 +5,5 @@ export * from './definitions/tSuggestion';
 export * from './definitions/tSuggestionReplaceFunction';
 export * as SuggestionFactory from './suggestionFactory';
 export * from './suggestionProvider';
-export * from './suggestionUtils';
 export * from './useCases/createSuggestions';
+export * from './utils/defaultReplaceFn';
